2c596a1399caa906b20278bb9448f13eeb846ef9
kasiwamotchi <totopomu@gamil.com>
2023-01-12 13:57:51 +0900

    add  tomorrow

--- TODOLIST/TODOlist.js
+++ TODOLIST/TODOlist.js
@@ -1,6 +1,8 @@
 const taskValue = document.getElementsByClassName('task_value')[0];
 const taskSubmit = document.getElementsByClassName('task_submit')[0];
+const tomorrow_task_submit= document.getElementsByClassName('tomorrow_task_submit')[0];
 const taskList = document.getElementsByClassName('task_list')[0];
+const tomorrow_taskList= document.getElementsByClassName('tomorrow_task_list')[0];
 
 '// 追加ボタンを作成//'
 const addTasks = (task) => {
@@ -34,3 +36,38 @@ taskSubmit.addEventListener('click', evt => {
   addTasks(task);
   taskValue.value = '';
 });
+
+  const tomorrow_addTasks = (tomorrow_task) => {
+// 入力したタスクを追加・表示
+    const tomorrow_listItem = document.createElement('li');
+    const tomorrow_showItem = tomorrow_taskList.appendChild(tomorrow_listItem);
+    tomorrow_showItem.innerHTML = tomorrow_task;
+
+      // タスクに削除ボタンを付与
+    const tomorrow_deleteButton = document.createElement('button');
+    tomorrow_deleteButton.innerHTML = 'Delete';
+    tomorrow_listItem.appendChild(tomorrow_deleteButton);
+
+      // 削除ボタンをクリックし、イベントを発動（タスクが削除）
+
+    tomorrow_deleteButton.addEventListener('click', evt => {
+    evt.preventDefault();
+    tomorrow_deleteTasks(tomorrow_deleteButton);
+    });
+
+  };
+
+  // 削除ボタンにタスクを消す機能を付与
+
+  const tomorrow_deleteTasks = (tomorrow_deleteButton) => {
+    const tomorrow_chosenTask = tomorrow_deleteButton.closest('li');
+    tomorrow_taskList.removeChild(tomorrow_chosenTask);
+  };
+
+  // 追加ボタンをクリックし、イベントを発動（タスクが追加）
+  tomorrow_task_submit.addEventListener('click', evt => {
+    evt.preventDefault();
+    const tomorrow_task = taskValue.value;
+    tomorrow_addTasks(tomorrow_task);
+    taskValue.value = '';
+  });
